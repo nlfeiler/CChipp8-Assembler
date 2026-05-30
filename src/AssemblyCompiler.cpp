bf25cb4920366211d4af1245e71b7ae1c7d519e9
nlfeiler <263273043+nlfeiler@users.noreply.github.com263273043+nlfeiler@users.noreply.github.com>
2026-05-29 20:51:00 -0400

    Full ADD function asm

--- src/AssemblyCompiler.cpp
+++ src/AssemblyCompiler.cpp
@@ -9,7 +9,7 @@
 //Refactor some of the code (LOWEST PRIORITY)
 //Add some more error handling 
 
-std::vector<std::string> assemblyFunctions = {"CLS", "JP", "ADD"};
+std::vector<std::string> assemblyFunctions = {"CLS", "JP", "ADD", "SE"};
 
 //This function will check to see if a token is a label that has been created and will get its memory location if so, if not it returns zero
 int ifLabelGetMemoryLocation(std::vector<std::string> &labels, std::vector<int> &labelMemoryLocations, std::string &token){
@@ -40,7 +40,8 @@ void compile(std::vector<std::string> &tokens, std::vector<int> &opcodes, std::s
             //no opcode to assemble here
         }
 
-        if(std::find(assemblyFunctions.begin(), assemblyFunctions.end(), tokens[i]) != assemblyFunctions.end())
+        //TODO: Test the comment below somehow
+        if(std::find(assemblyFunctions.begin(), assemblyFunctions.end(), tokens[i]) != assemblyFunctions.end()) //im pretty sure this will work successfully for creating the rom memory locations for labels?
             romMemoryLocation += 2;
     }
 
@@ -59,20 +60,48 @@ void compile(std::vector<std::string> &tokens, std::vector<int> &opcodes, std::s
             opcode = 0x00E0;
             opcodeAssembled = true; //we have assembled our opcode
         }
+        else if(tokens[i] == "RET"){ //00EE
+            opcode == 0x00EE;
+            opcodeAssembled = true;
+        }
         else if(tokens[i] == "JP"){ //jump opcode 1NNN
             opcode = 0x1000;
 
             //determine if the next token is a memory label
             int labelMemoryLocation = ifLabelGetMemoryLocation(labels, labelMemoryLocations, tokens[i+1]);
             if(labelMemoryLocation > -1) opcode += labelMemoryLocation;
+            else opcode += std::stoi(tokens[i+1], nullptr, 16);
 
             opcodeAssembled = true;
-        }else if(tokens[i] == "ADD"){
-            //TODO: make this full assembly opcode work for incrememnting the register by a byte and by a register and incrementing I
-            //right now this is just implementing incrementing a register by a byte
-            opcode = 0x7000;
-            opcode += (std::stoi(tokens[i+1], nullptr, 16) << 8) + (std::stoi(tokens[i+2], nullptr, 16) << 4); //assemble the opcode and for now just assume everything is hexadecimal for now
+        }else if(tokens[i] == "CALL"){ //2NNN
+            opcode = 0x2000;
+
+            //determine if the next token is a memory label
+            int labelMemoryLocation = ifLabelGetMemoryLocation(labels, labelMemoryLocations, tokens[i+1]);
+            if(labelMemoryLocation > -1) opcode += labelMemoryLocation;
+            else opcode += std::stoi(tokens[i+1], nullptr, 16);
+
             opcodeAssembled = true;
+        }else if(tokens[i] == "SE"){ //3XNN
+            opcode = 0x3000;
+
+            opcode += (std::stoi(std::string(1, tokens[i+1][1]), nullptr, 16) << 8) + std::stoi(tokens[i+2]);
+            opcodeAssembled = true;
+        }
+        else if(tokens[i] == "ADD"){
+            //TODO: some better way of doing what is below
+            //probably just get what the first char of each token is to determine so i dont have to do all the stupid stuff below
+            opcodeAssembled = true;
+            if(tokens[i+1][0] == 'V'){//increment register
+                if(tokens[i+2][0] == 'V'){
+                    opcode = 0x8000 + (std::stoi(std::string(1,tokens[i+1][1]), nullptr, 16) << 8) + (std::stoi(std::string(1, tokens[i+2][1]), nullptr, 16) << 4) + 4; //assemble the opcode and for now just assume everything is hexadecimal for now
+                }else{
+                    opcode = 0x7000 + (std::stoi(std::string(1,tokens[i+1][1]), nullptr, 16) << 8) + (std::stoi(tokens[i+2], nullptr, 16) << 4); //assemble the opcode and for now just assume everything is hexadecimal for now
+                }
+            }else if(tokens[i+1] == "I"){//increment I by register
+                opcode = 0xF000 + ((std::stoi(std::string(1,tokens[i+2][1]), nullptr, 16) << 8) + 0x1E);
+            }else
+                opcodeAssembled = false;
         }
         else{ //if we cannot recognize the token based off the rules above then we do not have an opcode to push to our list of assembled opcodes 
             opcodeAssembled = false;
